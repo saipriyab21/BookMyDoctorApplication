540bb6421b718c5a01b25f841285090f8c5d5538
saipriyab21 <54838316+saipriyab21@users.noreply.github.com>
2021-07-13 19:34:08 +0530

    Create appointment.service.ts

---
+++ BookMyDoctorApp2/FrontEnd/src/app/model/appointment.service.ts
@@ -0,0 +1,10 @@
+export class Appointment {
+    public appointmentId: number;
+    //public patient: Patient;
+    //public doctor: Doctor;
+    public appointmentDate: Date;
+    public appointmentStatus: String;
+    public remark: String;
+
+    constructor() { }
+}
